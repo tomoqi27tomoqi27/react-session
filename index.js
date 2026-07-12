8126653e25c4875e1268309e1e154eea52dae71c
tomoqi27tomoqi27 <tomoqi27@gmail.com>
2026-07-12 21:57:09 +0900

    section4

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-var var1 = 'var変数';
-console.log(var1);
+// var var1 = 'var変数';
+// console.log(var1);
+// // var変数は上書き可能
+// var1 = "var変数を上書き";
+// console.log(var1);
+// // var変数は再宣言可能
+// var var1 = "var変数を再宣言";
+// console.log(var1);
+// // できてなくね？？
+
+// let val2 = 'let変数';
+// console.log(val2);
+
+// // let変数は上書き可能
+// val2 = 'let変数を上書き';
+// console.log(val2);
+
+// // let変数は再宣言不可能
+// // let val2 = 'let変数を再宣言';
+
+const val3 = 'const変数';
+console.log(val3);
+
+// const変数は上書き不可能
+// val3 = 'const変数を上書き';
+
+// const変数は再宣言不可能
+// const val3 = 'const変数を再宣言';    
