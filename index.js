403c8564f6fadae66ec280b29da4260d9af07f9c
tomoqi27tomoqi27 <tomoqi27@gmail.com>
2026-07-14 10:09:05 +0900

    4-4

--- index.js
+++ index.js
@@ -88,8 +88,83 @@
 
 /**
  * 分割代入
+//  */
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 31,
+// };      
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}さいです。`;
+// console.log(message1);
+
+// // 分割代入を用いた方法
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}さいです。`;
+// console.log(message2);
+
+// const myProfile = ['じゃけぇ', 31];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}さいです。`;
+// console.log(message3);
+
+// // 分割代入を用いた方法
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}さいです。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数分割代入
  */
-const myProfile = {
-  name: "じゃけぇ",
-  age: 31,
-};      
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello();
+
+// const myProfile = {
+//   age: 31
+// };
+// const {age, name = "ゲスト" }=myProfile;
+// console.log(age);
+// console.log(name);
+
+/**
+ * オブジェクトの省略記法
+ * 従来の記法
+ */
+// const name = "じゃけぇ";
+// const age = 31;
+
+// const myProfile = {
+//   name: name,
+//   age: age,
+// };
+// console.log(myProfile);
+
+// // オブジェクトの省略記法を用いた方法→オブジェクトの定義しているプロパティの名前と同じ変数名を使う場合は省略できる
+// const myProfile2 = {
+//   name,
+//   age,
+// };
+// console.log(myProfile2);
+
+/**
+ * スプレッド構文 ...
+ */
+// 配列の展開
+
+// console.log(arr1);
+// console.log(...arr1);
+// const arr1 = [1, 2];
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1); 
+
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+const arr6 = [...arr4, ...arr5];
+console.log(arr6);
