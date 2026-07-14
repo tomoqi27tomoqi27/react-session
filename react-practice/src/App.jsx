8c588806b1a9f7ae8cca0c6018a08cf7b537cbcb
tomoqi27tomoqi27 <tomoqi27@gmail.com>
2026-07-14 23:12:18 +0900

    section6-47

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,8 +1,18 @@
 export const App = () => {
+  const onClickButton = () => alert('ボタンがクリックされました！');
+  const contentStyleA = {
+    color: 'blue',
+    fontSize: '20px'
+  };
+  const contentStyleB = {
+    color: 'red',
+    fontSize: '20px'
+  };
   return (
     <>
-  <h1>こんにちは</h1>
-  <p>お元気ですか</p>
-  </>
-);
-}
+      <h1 style={ contentStyleA }>こんにちは</h1>
+      <p style={contentStyleB} >お元気ですか</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
